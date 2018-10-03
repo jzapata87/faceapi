27898cde9a6fe34f567f84da6a5d45fb2edfff77
Javier Zapata <zapataj87@gmail.com>
2018-10-03 14:36:57 -0500

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,35 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'b8fc8f0dee834dce8aef7933d8f4fedc'
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with api'))
+}
+
+
+
+
+const handleImage = (req,res, db) => {
+ const { id } = req.body;
+ db('users').where('id', '=', id)
+ .increment('entries', 1)
+ .returning('entries')
+ .then(entries => {
+   res.json(entries[0]);
+ })
+ .catch(err => res.status(400).json('unable to get count'))
+
+}
+
+module.exports = {
+  handleImage,
+  handleApiCall
+}
